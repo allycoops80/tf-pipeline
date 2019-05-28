320bc0f082aa6ef4c8d55928fa6819d590e54740
Alan Cooper <alan.cooper@skyscanner.net>
2019-05-28 16:45:39 +0100

    Correcting bucketname

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "bucket=infx-tf-codepipeline-terraformstatebucket-1fd9kzua1hz9v"
+    bucket  = "infx-tf-codepipeline-terraformstatebucket-1fd9kzua1hz9v"
     key     = "dns-hub/dns-hub-tertiary.tfstate"
 
     region  = "eu-west-1"
